6097adf4d33060d6d9e0106ea5545b9b0425c7c9
markporo <markporo@yahoo.com>
2022-07-08 01:26:27 -0400

    footer contact relief

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -7,10 +7,10 @@ import Footer from "../components/Footer";
 
 export default function Contact() {
   return (
-    <div className="">
+    <div className="flex flex-col justify-between">
       <Navbar />
       <div className="flex flex-col justify-center items-center">
-        <form className=" rounded-lg mb-12 mt-36 text-pwhite bg-bblue w-2/5 min-w-[260px] text-center h-[325px] flex flex-col justify-center items-center" id="contact" name="contact" method="POST" data-netlify="true">
+        <form className=" rounded-lg mb-36 mt-36 text-pwhite bg-bblue w-2/5 min-w-[260px] text-center h-[325px] flex flex-col justify-center items-center" id="contact" name="contact" method="POST" data-netlify="true">
           <label>Contact Me</label>
 
           <input type="text" name="name" placeholder="  Your Name" />
@@ -21,7 +21,7 @@ export default function Contact() {
           {/* <label>Message Me:</label>   */}
           <textarea name="message" placeholder=" Write me a message!"></textarea>
           
-          <button className=" z-40 h-12 hover:scale-105 bg-gradient-to-r from-newgreen to-pwhite hover:bg-gradient-to-r hover:from-pwhite hover:to-newgreen mt-4 text-gray-700 w-3/4 shadow-xl" type="submit">
+          <button className=" z-40 h-10 hover:scale-105 bg-gradient-to-r from-newgreen to-pwhite hover:bg-gradient-to-r hover:from-pwhite hover:to-newgreen mt-4 text-gray-700 w-3/4 shadow-xl" type="submit">
             Send
           </button> 
 
